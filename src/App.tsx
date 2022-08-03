e93ba0e0b4e37482bfd569bd5a4ea45dbe54c7d2
Milton Navarro <milton.j.navarro.p@gmail.com>
2022-08-02 21:27:29 -0600

    add material ui to react js application

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
